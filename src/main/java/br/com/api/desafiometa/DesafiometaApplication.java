461c743a2554a31fc40941c5ed211f7be7d07369
George Paiva <george.paiva15@gmail.com>
2020-06-22 09:58:15 -0300

    first commit

---
+++ src/main/java/br/com/api/desafiometa/DesafiometaApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.desafiometa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiometaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiometaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/desafiometa/DesafiometaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.desafiometa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiometaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
